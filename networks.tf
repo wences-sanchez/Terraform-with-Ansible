a957b035b1c9fcd17477c793b7fcefefa7c438e0
wences-sanchez <wenceslaosanchezpino@gmail.com>
2022-03-23 08:36:18 +0100

    Add second VPC

--- networks.tf
+++ networks.tf
@@ -7,3 +7,12 @@ resource "aws_vpc" "vpc_master" {
     Name = "Master-VPC-jenkins"
   }
 }
+
+resource "aws_vpc" "vpc_worker" {
+  cidr_block           = "192.168.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "Worker-VPC-jenkins"
+  }
+}

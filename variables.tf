07e529bb0c9ba9d582fd07ff52d3499035966654
wences-sanchez <wenceslaosanchezpino@gmail.com>
2022-03-23 10:37:38 +0100

    Add variable for SSM dynamic AMI

--- variables.tf
+++ variables.tf
@@ -9,3 +9,11 @@ variable "region-eu-south" {
   type        = string
   default     = "eu-south-1"
 }
+
+variable "ssm-al2-ami" {
+  description = "SSM Label of the AMI for the latest version of Amazon Linux 2"
+  # This label has been created by the command 
+  # `$ aws ssm get-parameters --names /aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2 --region eu-south-1`
+  type    = string
+  default = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+}

5fa8438e990d41a10d995a1b3dcedb14b90575fc
wences-sanchez <wenceslaosanchezpino@gmail.com>
2022-03-23 08:30:01 +0100

    Add variables
    Added variable profile with default value
    Added variable region-eu-south with the code
    for the Milan Region

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "profile" {
+  description = "This is the default profile for aws"
+  type        = string
+  default     = "default"
+}
+
+variable "region-eu-south" {
+  description = "AWS Region for Milan"
+  type        = string
+  default     = "eu-south-1"
+}
